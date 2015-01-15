e6fb171801c5a0d45d326fa796eb81a31e180607
Paige <paigegu3+git@gmail.com>
2015-01-14 19:23:17 -0700

    learning js

--- practice.js
+++ practice.js
@@ -4,24 +4,28 @@
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
-
   //Code Here
+var isTyler = function(name) {
+  if (name == 'Tyler') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isTyler(name);
 
 
 //Next problem
-
-
-
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
-
   //Code Here
+var getName = function() {
+  var name = prompt("What's your name?");
+  return name;
+}
 
 
 //Next Problem
-
-
-
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
