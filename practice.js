5e455032fdf0c4d3625168034ac1372d15af9d35
Paige <paigegu3+git@gmail.com>
2015-01-14 19:45:55 -0700

    more js practice bits

--- practice.js
+++ practice.js
@@ -11,7 +11,7 @@ var isTyler = function(name) {
   } else {
     return false;
   }
-}
+};
 isTyler(name);
 
 
@@ -21,7 +21,7 @@ isTyler(name);
 var getName = function() {
   var name = prompt("What's your name?");
   return name;
-}
+};
 getName();
 
 
@@ -32,7 +32,7 @@ getName();
 var welcome = function() {
   var name = getName();
   return "Welcome, " + name;
-}
+};
 welcome();
 
 
@@ -48,38 +48,55 @@ var adder = function() {
     theTotal = theTotal + arguments[i];
   }
   return "The total number was " + theTotal;
-}
+};
 adder(1, 2, 3);
 
 
 //5. Next Problem
 //What is the difference between arguments and parameters?
   //Answer Here
-
+Parameters are set up in the function definition, behave like local variables
+Arguments are passed into the function when the function is invoked
 
 
 //6. Next problem
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
   //Answer Here
-
+falsy is undefined, null, 0, '', NaN, none
+!!! check if something is falsy by 
 
 
 //7. Next Problem
 //Create a function called myName that returns your name
   //Code Here
+var myName = function() {
+  return "Paige";
+};
 
 //Now save the function definition of myName into a new variable called newMyName
   //Code Here
+newMyName = myName;
 
 //Now alert the result of invoking newMyName
+newMyName();
 
 
 
 //8. Next problem
 //Create a function called outerFn which returns an anonymous function which returns your name.
   //Code Here
-
+var outerFn = function() {
+  function() {
+    return "Paige";
+  }
+};
 //Now save the result of invoking outerFn into a variable called innerFn.
   //Code Here
 
+
 //Now invoke innerFn.
+
+
+
+
+
